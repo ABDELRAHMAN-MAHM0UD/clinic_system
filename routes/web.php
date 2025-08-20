7e755006b4af7a90870483c70e47719ee41bef4a
nourien mohammed <norienmohammed052@gmail.com>
2025-08-20 20:51:35 +0300

    Added user sign up (controller, blade, routes)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
-
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/signup', [AuthController::class, 'showSignupForm'])->name('signup.form');
+Route::post('/signup', [AuthController::class, 'signup'])->name('signup');
