cc8b47e81b22a8c70e6eb0f2f37a1cd90e4a9685
ABDELRAHMAN-MAHM0UD <abdelrahman._.mahmoud@hotmail.com>
2025-08-24 23:58:25 +0300

    arranged DB

--- database/migrations/2025_08_24_203957_renaming.php
+++ database/migrations/2025_08_24_203957_renaming.php
@@ -10,19 +10,18 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::rename('users', 'patients');
-
+      {
+         
         // First drop the foreign key from appointments
         Schema::table('appointments', function (Blueprint $table) {
             $table->dropForeign(['patient_id']); // remove FK constraint
-            $table->foreign('patient_id')->references('id')->on('patients')->onDelete('cascade');
+            $table->foreign('patient_id')->references('id')->on('users')->onDelete('cascade');
         });
-        // Now drop the patients table
-        Schema::dropIfExists('patient');        
+        Schema::dropIfExists('patient');
+             
     }
 
-    /**
+    /**x
      * Reverse the migrations.
      */
     public function down(): void
